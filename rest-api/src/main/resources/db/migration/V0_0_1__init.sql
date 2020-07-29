5f0afb1346f877d7431411f09db7efa883b4f954
Darius Tack <tack@smartsquare.de>
2020-07-29 16:55:23 +0200

    Add postgresql with docker compose file and flyway

---
+++ rest-api/src/main/resources/db/migration/V0_0_1__init.sql
@@ -0,0 +1,9 @@
+create table pull_request
+(
+    id         bigint      not null primary key,
+    title      varchar     not null,
+    url        varchar     not null,
+    created_at timestamptz not null,
+    closed_at  timestamptz not null,
+    merged_at  timestamptz
+);
