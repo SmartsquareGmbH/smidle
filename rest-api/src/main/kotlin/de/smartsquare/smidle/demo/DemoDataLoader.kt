429df50c1a2c940a1a5535832c9f9df320121c88
lschuer <schuerstedt@smartsquare.de>
2020-08-07 17:50:10 +0200

    More demo data

--- rest-api/src/main/kotlin/de/smartsquare/smidle/demo/DemoDataLoader.kt
+++ rest-api/src/main/kotlin/de/smartsquare/smidle/demo/DemoDataLoader.kt
@@ -61,8 +61,71 @@ class DemoDataLoader(private val pullRequestRepository: PullRequestRepository) :
             title = "Fantastic feature",
             url = "https://github.com/",
             createdAt = Instant.parse("2020-07-16T10:34:19Z"),
+            closedAt = Instant.parse("2020-07-16T13:01:59Z"),
+            mergedAt = Instant.parse("2020-07-16T13:01:59Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147431,
+            title = "Another feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-15T10:34:19Z"),
             closedAt = Instant.parse("2020-07-16T15:57:30Z"),
             mergedAt = Instant.parse("2020-07-16T15:57:30Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147430,
+            title = "Nice feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-14T11:23:47Z"),
+            closedAt = Instant.parse("2020-07-16T18:06:19Z"),
+            mergedAt = Instant.parse("2020-07-16T18:06:19Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147429,
+            title = "Useful feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-14T10:48:27Z"),
+            closedAt = Instant.parse("2020-07-15T09:06:13Z"),
+            mergedAt = Instant.parse("2020-07-15T09:06:13Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147428,
+            title = "Boring feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-14T10:02:40Z"),
+            closedAt = Instant.parse("2020-07-14T11:36:22Z"),
+            mergedAt = Instant.parse("2020-07-14T11:36:22Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147427,
+            title = "One more feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-10T15:11:28Z"),
+            closedAt = Instant.parse("2020-07-14T08:48:14Z"),
+            mergedAt = Instant.parse("2020-07-14T08:48:14Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147426,
+            title = "A feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-12T13:30:17Z"),
+            closedAt = Instant.parse("2020-07-13T16:21:19Z"),
+            mergedAt = Instant.parse("2020-07-13T16:21:19Z")
+        ))
+
+        pullRequestRepository.save(PullRequest(
+            id = 279147425,
+            title = "THE feature",
+            url = "https://github.com/",
+            createdAt = Instant.parse("2020-07-13T09:19:56Z"),
+            closedAt = Instant.parse("2020-07-13T10:56:43Z"),
+            mergedAt = Instant.parse("2020-07-13T10:56:43Z")
         ))
     }
 }
