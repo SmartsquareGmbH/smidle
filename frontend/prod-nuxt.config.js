1688ad4177a546a83e225af9e92fcea7f505ce75
lschuer <schuerstedt@smartsquare.de>
2020-08-11 10:56:39 +0200

    Add docker file for frontend and build image with github actions

---
+++ frontend/prod-nuxt.config.js
@@ -0,0 +1,65 @@
+import colors from "vuetify/es5/util/colors"
+
+export default {
+  mode: "spa",
+  head: {
+    title: "Smidle",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  pwa: {
+    manifest: {
+      theme_color: "#35B0AE",
+      name: "Smidle",
+      short_name: "Smidle",
+    },
+    meta: {
+      name: "Smidle",
+      ogHost: "smidle.smartsquare.de",
+    },
+  },
+  css: ["typeface-roboto/index.css", "@mdi/font/css/materialdesignicons.css"],
+  plugins: [],
+  components: true,
+  buildModules: ["@nuxtjs/eslint-module", "@nuxtjs/vuetify"],
+  modules: ["@nuxtjs/axios"],
+  axios: {
+    baseURL: "/",
+  },
+  loading: { color: "#26A69A" },
+  loadingIndicator: {
+    name: "pulse",
+    color: "#26A69A",
+    background: "white",
+  },
+  layoutTransition: {
+    name: "fade-transition",
+  },
+  pageTransition: {
+    name: "slide-x-transition",
+  },
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: "#26A69A",
+          secondary: "#C2185B",
+          accent: colors.grey.lighten1,
+        },
+      },
+    },
+    defaultAssets: {
+      icons: false,
+      font: false,
+    },
+  },
+  build: {},
+}
